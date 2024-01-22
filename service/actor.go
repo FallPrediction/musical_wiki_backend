6dbd4911cac850ed7d837ea6a7732f1259b2c435
ElizabethHuang <lizne6z0@gmail.com>
2024-01-22 22:15:12 +0800

    Set or get cache when get or update an actor.

--- service/actor.go
+++ service/actor.go
@@ -1,9 +1,14 @@
 package service
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"musical_wiki/global"
 	"musical_wiki/models"
 	"musical_wiki/repository"
 	"musical_wiki/request"
+	"time"
 )
 
 type ActorService struct {
@@ -15,7 +20,29 @@ func (service *ActorService) Index() ([]models.Actor, error) {
 }
 
 func (service *ActorService) Show(id string) (models.Actor, error) {
-	return service.repo.Show(id)
+	ctx := context.Background()
+	key := fmt.Sprintf("actor:%v", id)
+	bytes, err := global.Redis.Get(ctx, key).Bytes()
+	if err == nil {
+		var cacheActor models.Actor
+		err = json.Unmarshal(bytes, &cacheActor)
+		if err != nil {
+			global.Logger.Warn("json unmarshal error", err)
+		} else {
+			return cacheActor, nil
+		}
+	}
+
+	actor, actorErr := service.repo.Show(id)
+	if actorErr == nil {
+		bytes, err = json.Marshal(actor)
+		if err != nil {
+			global.Logger.Warn("json marshal error", err)
+		} else {
+			global.Redis.Set(ctx, key, bytes, 24*time.Hour)
+		}
+	}
+	return actor, actorErr
 }
 
 func (service *ActorService) Store(request *request.Actor) (models.Actor, error) {
@@ -43,6 +70,11 @@ func (service *ActorService) Update(id string, request *request.Actor) error {
 		Content:        request.Content,
 		Socials:        request.Socials,
 	}
+
+	ctx := context.Background()
+	key := fmt.Sprintf("actor:%v", id)
+	global.Redis.Del(ctx, key)
+
 	return service.repo.Update(id, &actor)
 }
 
