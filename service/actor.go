87a6f637eac81d5b0dd43c7beb15154f6b4bd0bd
ElizabethHuang <lizne6z0@gmail.com>
2024-02-18 14:31:28 +0800

    Set or get cache when get, update, delete actors.

--- service/actor.go
+++ service/actor.go
@@ -8,6 +8,7 @@ import (
 	"musical_wiki/models"
 	"musical_wiki/repository"
 	"musical_wiki/request"
+	"strconv"
 	"time"
 )
 
@@ -16,7 +17,31 @@ type ActorService struct {
 }
 
 func (service *ActorService) Index(currentPage int, perPage int) ([]models.Actor, int64, error) {
-	return service.repo.Index(currentPage, perPage)
+	ctx := context.Background()
+	key := fmt.Sprintf("actorsList:size=%v:currPage=%v", perPage, currentPage)
+	cache, err := global.Redis.HGetAll(ctx, key).Result()
+	if err == nil {
+		var cacheActors []models.Actor
+		err = json.Unmarshal([]byte(cache["actors"]), &cacheActors)
+		if err != nil {
+			global.Logger.Warn("json unmarshal error", err)
+		} else {
+			count, _ := strconv.ParseInt(cache["count"], 10, 64)
+			return cacheActors, count, nil
+		}
+	}
+
+	actors, count, actorsErr := service.repo.Index(currentPage, perPage)
+	if actorsErr == nil {
+		bytes, err := json.Marshal(actors)
+		if err != nil {
+			global.Logger.Warn("json marshal error", err)
+		} else {
+			global.Redis.HSet(ctx, key, "actors", bytes, "count", count)
+			global.Redis.Expire(ctx, key, 24*time.Hour)
+		}
+	}
+	return actors, count, actorsErr
 }
 
 func (service *ActorService) Show(id string) (models.Actor, error) {
@@ -71,13 +96,27 @@ func (service *ActorService) Update(id string, request *request.Actor) error {
 		Socials:        request.Socials,
 	}
 
-	ctx := context.Background()
-	key := fmt.Sprintf("actor:%v", id)
-	global.Redis.Del(ctx, key)
-
+	service.delActorCache(id)
+	service.delActorsListCache()
 	return service.repo.Update(id, &actor)
 }
 
 func (service *ActorService) Destroy(id string) error {
+	service.delActorCache(id)
+	service.delActorsListCache()
 	return service.repo.Destroy(id)
+}
+
+func (service *ActorService) delActorCache(id string) {
+	ctx := context.Background()
+	key := fmt.Sprintf("actor:%v", id)
+	global.Redis.Del(ctx, key)
+}
+
+func (service *ActorService) delActorsListCache() {
+	ctx := context.Background()
+	iter := global.Redis.Scan(ctx, 0, "actorsList:size=*:currPage=*", 0).Iterator()
+	for iter.Next(ctx) {
+		global.Redis.Del(ctx, iter.Val())
+	}
 }
