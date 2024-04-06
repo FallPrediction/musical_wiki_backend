37d2ee64573e9d7b27822907b4c0758c7d354150
Elizabeth Huang <elizabeth.huang@kkday.com>
2024-04-06 14:18:00 +0800

    refactor: S3 initialize.

--- config/s3.go
+++ config/s3.go
@@ -1,9 +1,11 @@
 package config
 
 import (
-	_ "github.com/joho/godotenv/autoload"
 	"os"
+
+	_ "github.com/joho/godotenv/autoload"
 )
 
 var Bucket = os.Getenv("AWS_BUCKET")
 var Endpoint = os.Getenv("AWS_ENDPOINT")
+var Region = os.Getenv("AWS_REGION")
--- initialize/s3.go
+++ initialize/s3.go
@@ -2,6 +2,7 @@ package initialize
 
 import (
 	"context"
+	"musical_wiki/config"
 	"sync"
 
 	awsConfig "github.com/aws/aws-sdk-go-v2/config"
@@ -15,7 +16,7 @@ var s3Once sync.Once
 func NewS3(logger *zap.SugaredLogger) *s3.Client {
 	if s3Client == nil {
 		s3Once.Do(func() {
-			cfg, err := awsConfig.LoadDefaultConfig(context.TODO())
+			cfg, err := awsConfig.LoadDefaultConfig(context.TODO(), awsConfig.WithRegion(config.Region))
 			if err != nil {
 				logger.Error("unable to load SDK config", err)
 			}
