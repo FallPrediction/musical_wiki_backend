1dc5133d8100c226725062ef0e6ab8f087052548
ElizabethHuang <lizne6z0@gmail.com>
2024-04-18 22:36:44 +0800

    chore: Modify command script for CompileDaemon.

--- deployments/local/kill_and_debug.sh
+++ deployments/local/kill_and_debug.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-kill $(ps aux | grep '[d]lv exec' | awk '{print $1}')
+kill -SIGINT $(ps aux | grep '[d]lv exec' | awk '{print $1}')
 dlv exec --headless --continue --listen :4040 --accept-multiclient ./musical_wiki
